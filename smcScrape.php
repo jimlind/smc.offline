18da456ebdfbb0a2b6a97c604b3e5a907961f61b
Jim Lind <spoon.vw@gmail.com>
2012-02-16 23:14:57 -0600

    A bit of something to pull pages from different domains in is required because XSS deterrants stop JavaScript from being able to do it. This is just rubbish code.

---
+++ smcScrape.php
@@ -0,0 +1,33 @@
+<?php
+/* To avoid abuse, only allow scraping of StashMyComics */
+
+// Just to be nice to the guys at StashMyComics.
+sleep(0.5);
+
+// Setup the request
+$baseURL = 'http://www.stashmycomics.com/html';
+
+// Sanitize input
+$i = filter_input(INPUT_GET, 'i', FILTER_SANITIZE_STRING);
+// Decode input
+$i = urldecode($i);
+// Remove all characters that are weird
+$i = preg_replace('/[^a-z0-9:& .?=-]/i', '', $i);
+
+// Replace space with plus;
+$i = str_replace(' ', '+', $i);
+
+// I've never been able to trust the PHP-cURL library, so I do it the hard way.
+$fetchScript = "curl '$baseURL/$i'";
+$output = shell_exec($fetchScript);
+if($output == null) {
+    echo "FAILURE TO COMMUNICATE";
+    die;
+}
+// Remove some bits of unneccesary HTML
+$output = preg_replace('/<head(.*?)head>/s', '', $output);	// Remove the HTMl Header
+$output = preg_replace('/<script(.*?)script>/s', '', $output);	// Remove all Scripts
+$output = preg_replace('/<form(.*?)form>/s', '', $output);	// Remove all Forms
+
+//var_dump($fetchScript);
+echo $output;
